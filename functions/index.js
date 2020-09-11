3beaa9937003356567348e4ae6f08a64abc6679a
ajmalfaiz <ajmalfaiz5@gmail.com>
2020-09-11 19:46:31 +0530

    working on backend stage 1

--- functions/index.js
+++ functions/index.js
@@ -9,12 +9,25 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 app.use(express.json());
 
 //API Routes
 
-app.get('/', (req,res) => res.status(200).send('Hello it works yeahhhh'))
+app.get('/', (req, res) => res.status(200).send('Hello it works yeahhhh'))
+app.get('/aju', (req, res) => res.status(200).send('اه شتةع'))
+app.post('/payments/create', async (req, res) => {
+    const total = req.query.total
+    console.log('Payment request recieved yeeee', total)
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "inr"
+    })
+    res.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+}
+)
 
 // Listen Command
 exports.api = functions.https.onRequest(app)
